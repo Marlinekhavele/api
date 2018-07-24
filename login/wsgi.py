b7d4b988d7f51ba1464c25331134bea10943f77b
Your Name <you@example.com>
2018-07-24 18:28:45 +0300

    modified wsgi file

--- login/wsgi.py
+++ login/wsgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
+import dev
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "login.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "login.settings.dev")
 
 application = get_wsgi_application()
